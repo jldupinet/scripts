207b3353397a69290aeb9b687aa22a1a402e6391
Jose Luis Dupinet Diosdado <jldupinet@gmail.com>
2016-05-29 13:15:27 -0500

    Removes dependency of get_db_dump.sh to gen_db_dump.sh

--- get_db_dump.sh
+++ get_db_dump.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# TODO implement key-value args
+# TODO implement args for username and home path in remote server
+#
+# for the moment:
+# $1 name of remote server (must be declared in ssh-config)
+# $2 name of remote DB
 OF=$(date +%Y%m%d).dump
-ssh $1 'bash -s' < gen_db_dump.sh $2
+ssh $1 'bash -s' < 'pg_dump -Fc -O --host=localhost --port=5432 --username=ubuntu --dbname=$1 > $OF' $2
 rsync -vzar $1:/home/ubuntu/$OF . 
